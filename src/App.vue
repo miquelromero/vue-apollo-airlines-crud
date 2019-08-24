266f807a5b88cfb783336b37532074203393bbc6
Miquel Romero Sanfeliu <miquelromero@outlook.com>
2019-08-24 12:45:56 +0200

    Persist selected language in localstorage and vuex

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
     <b-navbar type="dark" variant="primary">
-      <b-navbar-brand :to="{ name: 'home' }">Airlines crud</b-navbar-brand>
+      <b-navbar-brand :to="{ name: 'home' }">{{ $t('title') }}</b-navbar-brand>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown :text="$i18n.locale.toLocaleUpperCase()" right>
+          <b-nav-item-dropdown :text="this.lang.toLocaleUpperCase()" right>
             <b-dropdown-item
               href="#"
               v-for="lang in langs"
-              :key="lang" @click="setCurrentLocale(lang)"
+              :key="lang" @click="changeLanguage(lang)"
             >
               {{ lang.toLocaleUpperCase() }}
             </b-dropdown-item>
@@ -23,15 +23,29 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex';
+
 export default {
   data() {
     return {
       langs: Object.keys(this.$i18n.messages),
     };
   },
+  computed: {
+    ...mapState([
+      'lang',
+    ]),
+  },
+  created() {
+    this.changeLanguage(this.lang);
+  },
   methods: {
-    setCurrentLocale(lang) {
+    ...mapMutations([
+      'setLanguage',
+    ]),
+    changeLanguage(lang) {
       this.$i18n.locale = lang;
+      this.setLanguage(lang);
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -1,16 +1,27 @@
+/* eslint-disable no-param-reassign */
+
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const LOCALSTORAGE_LANG_KEY = 'language';
+const getBrowserLanguage = () => {
+  const browserLanguage = window.navigator.language;
+  if (browserLanguage) {
+    return browserLanguage.split('-')[0];
+  }
+  return null;
+};
+
 export default new Vuex.Store({
   state: {
-
+    lang: window.localStorage.getItem(LOCALSTORAGE_LANG_KEY) || getBrowserLanguage() || 'en',
   },
   mutations: {
-
-  },
-  actions: {
-
+    setLanguage(state, lang) {
+      window.localStorage.setItem(LOCALSTORAGE_LANG_KEY, lang);
+      state.lang = lang;
+    },
   },
 });
