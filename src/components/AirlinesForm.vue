d504b54d740a9b19d21574e1327331b3eb2daa47
Miquel Romero Sanfeliu <miquelromero@outlook.com>
2019-08-25 11:56:14 +0200

    Add color picker to airlines form and fix localizations

--- src/components/AirlinesForm.vue
+++ src/components/AirlinesForm.vue
@@ -20,6 +20,7 @@
     <b-form-group :label="$t('airlines.form.primaryColor.label')" label-for="primary_color">
       <b-form-input
         id="primary_color"
+        type="color"
         v-model="form.primary_color"
         required
         :placeholder="$t('airlines.form.primaryColor.placeholder')"
@@ -28,22 +29,24 @@
     <b-form-group :label="$t('airlines.form.secondaryColor.label')" label-for="secondary_color">
       <b-form-input
         id="secondary_color"
+        type="color"
         v-model="form.secondary_color"
         required
         :placeholder="$t('airlines.form.secondaryColor.placeholder')"
       />
     </b-form-group>
-    <b-form-group label="Using sub-components:">
-      <b-form-checkbox-group id="checkbox-group-2" v-model="form.services" name="flavour-2">
-        <b-form-checkbox v-for="service in services" :key="service.key" :value="service.key">{{
-          service.key
-        }}</b-form-checkbox>
+    <b-form-group :label="$t('airlines.form.services.label')">
+      <b-form-checkbox-group id="services" v-model="form.services" name="services">
+        <b-form-checkbox v-for="service in services" :key="service.key" :value="service.key">
+          {{ $t(`services.${service.key}`) }}
+        </b-form-checkbox>
       </b-form-checkbox-group>
     </b-form-group>
   </b-form>
 </template>
 
 <script>
+import Color from 'color';
 import services from '@/assets/js/services';
 
 export default {
@@ -62,19 +65,21 @@ export default {
   methods: {
     initData() {
       if (this.item) {
+        const primaryColor = Color(this.item.primary_color);
+        const secondaryColor = Color(this.item.secondary_color);
         this.form = {
           iata: this.item.iata,
           name: this.item.name,
-          primary_color: this.item.primary_color,
-          secondary_color: this.item.secondary_color,
+          primary_color: primaryColor.hex(),
+          secondary_color: secondaryColor.hex(),
           services: [...this.item.services],
         };
       } else {
         this.form = {
           iata: '',
           name: '',
-          primary_color: '',
-          secondary_color: '',
+          primary_color: '#41b883',
+          secondary_color: '#35495e',
           services: [],
         };
       }
